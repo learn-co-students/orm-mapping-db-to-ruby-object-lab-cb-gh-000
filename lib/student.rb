ebeac93b8dd6f2392925e028aeb41c79fd65a139
robertjosephwayne <robertjwayne@gmail.com>
2017-04-22 22:05:34 -0400

    Add .find_by_name method to Student class

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,7 @@
 class Student
   attr_accessor :id, :name, :grade
 
-  # create a new Student object given a row from the database
+  # Create a new Student object given a row from the database
   def self.new_from_db(row)
     student = self.new
     student.id = row[0]
@@ -10,8 +10,7 @@ class Student
     student
   end
 
-  # retrieve all the rows from the "Students" database
-  # remember each row should be a new instance of the Student class
+  # Retrieve all the rows from the "Students" database
   def self.all
     sql = <<-SQL
       SELECT * FROM students
@@ -22,9 +21,18 @@ class Student
     end
   end
 
+  # Find the student in the database given a name
+  # Return a new instance of the Student class
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
   def save
