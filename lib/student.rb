4dc3bf0cd54108d39e8c6e84ad7fef98ff74d0bd
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-19 19:46:26 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -57,6 +57,44 @@ class Student
       WHERE grade = "9"
     SQL
 
-    DB[:conn].execute(sql, name).collect {|row| self.new_from_db(row)}
+    DB[:conn].execute(sql).collect {|row| self.new_from_db(row)}
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE NOT grade = "12"
+    SQL
+
+    DB[:conn].execute(sql).collect {|row| self.new_from_db(row)}
+  end
+
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = "10"
+      LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, x).collect {|row| self.new_from_db(row)}
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = "10"
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql).collect {|row| self.new_from_db(row)}[0]
+  end
+
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, x.to_s).collect {|row| self.new_from_db(row)}
   end
 end
